65d284777a61f4658c8656b788e112379487a712
Rafael da Luz Rodrigues <rafaeldaluzrodrigues@outlook.com.br>
2026-04-11 19:12:37 +0100

    fix: documentation with login

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -210,10 +210,6 @@ async function handleLogin() {
           <router-link to="/auth/register" class="login-link">Registe-se</router-link>
         </p>
         -->
-
-        <p class="login-doc-link mt-4 mb-0 text-center">
-          <router-link to="/documentation" class="login-link">Documentação do projeto</router-link>
-        </p>
       </div>
     </div>
 
